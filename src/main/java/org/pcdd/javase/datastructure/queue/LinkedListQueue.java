fca39663a7f7d5278af3400697c773ac463edcf1
pcdd <hello_pcdd@163.com>
2023-03-26 20:33:51 +0800

    :sparkles: 新增数据结构之队列Java示例代码

---
+++ src/main/java/org/pcdd/javase/datastructure/queue/LinkedListQueue.java
@@ -0,0 +1,102 @@
+package org.pcdd.javase.datastructure.queue;
+
+import java.util.Iterator;
+
+/**
+ * 数据结构：队列
+ * 单项环形哨兵链表实现
+ *
+ * @author pcdd
+ */
+public class LinkedListQueue<E> implements Queue<E> {
+
+    private static class Node<E> {
+        E value;
+        Node<E> next;
+
+        Node(E value, Node<E> next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    private Node<E> head = new Node<>(null, null);
+    private Node<E> tail = head;
+    private int size;
+    private int capacity = Integer.MAX_VALUE;
+
+    public LinkedListQueue() {
+        this.tail.next = head;
+    }
+
+    public LinkedListQueue(int capacity) {
+        this.capacity = capacity;
+        this.tail.next = head;
+    }
+
+    @Override
+    public boolean offer(E e) {
+        if (size == capacity) {
+            System.out.println("队列已满！");
+            return false;
+        }
+        Node<E> added = new Node<>(e, head);
+        tail.next = added;
+        tail = added;
+        size++;
+        return true;
+    }
+
+    @Override
+    public E poll() {
+        if (isEmpty()) {
+            return null;
+        }
+        // 第一个节点
+        Node<E> first = head.next;
+        // 删除第一个节点
+        head.next = first.next;
+        // 只有一个结点时
+        if (tail == first) {
+            // 删除后，tail需指向head
+            tail = head;
+        }
+        size--;
+        return first.value;
+    }
+
+    @Override
+    public E peek() {
+        return isEmpty() ? null : head.next.value;
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return head == tail;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<>() {
+            Node<E> p = head.next;
+
+            @Override
+            public boolean hasNext() {
+                return p != head;
+            }
+
+            @Override
+            public E next() {
+                E value = p.value;
+                p = p.next;
+                return value;
+            }
+        };
+    }
+
+}
---
+++ src/main/java/org/pcdd/javase/datastructure/queue/MainTest.java
@@ -0,0 +1,30 @@
+package org.pcdd.javase.datastructure.queue;
+
+/**
+ * @author pcdd
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        LinkedListQueue<Integer> queue = new LinkedListQueue<>(3);
+        System.out.println("linkedListQueue.isEmpty() = " + queue.isEmpty());
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(3);
+        queue.offer(4);
+        System.out.println("linkedListQueue.isEmpty() = " + queue.isEmpty());
+        print(queue);
+        System.out.println("linkedListQueue.size() = " + queue.size());
+
+        System.out.println("linkedListQueue.peek() = " + queue.peek());
+
+        System.out.println("linkedListQueue.poll() = " + queue.poll());
+        print(queue);
+        System.out.println("linkedListQueue.size() = " + queue.size());
+    }
+
+    static void print(LinkedListQueue<Integer> linkedListQueue) {
+        for (Integer integer : linkedListQueue) {
+            System.out.println("Node value: " + integer);
+        }
+    }
+}
