940c88f0ffff16748bad9a2edd482a8385975204
pcdd <hello_pcdd@163.com>
2023-03-30 01:27:35 +0800

    :art: Update JdbcUtils.java

--- src/main/java/org/pcdd/javase/tutorial/chapter20_数据库编程/JdbcUtils.java
+++ src/main/java/org/pcdd/javase/tutorial/chapter20_数据库编程/JdbcUtils.java
@@ -1,6 +1,7 @@
 package org.pcdd.javase.tutorial.chapter20_数据库编程;
 
 import com.alibaba.druid.pool.DruidDataSourceFactory;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.sql.DataSource;
 import java.io.IOException;
@@ -16,15 +17,19 @@ import java.util.Properties;
  * @author pcdd
  * date 2020/12/19 22:00
  */
+@Slf4j
 public class JdbcUtils {
 
-    public static DataSource ds = null;
-    public static Connection con = null;
-    public static PreparedStatement pstmt = null;
-    public static ResultSet rs = null;
+    private static DataSource ds = null;
+    private static Connection con = null;
+    private static PreparedStatement pstmt = null;
+    private static ResultSet rs = null;
 
     static {
         init();
+    }
+
+    private JdbcUtils() {
     }
 
     public static void init() {
@@ -35,17 +40,46 @@ public class JdbcUtils {
             ds = DruidDataSourceFactory.createDataSource(pro);
             con = ds.getConnection();
         } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("jdbc配置文件加载失败！");
+            log.error("jdbc配置文件加载失败：{}", e.getMessage(), e);
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            System.out.println("数据库驱动加载失败！");
+            log.error("数据库驱动加载失败：{}", e.getMessage(), e);
         } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("创建数据源失败！");
+            log.error("JdbcUtils初始化失败：{}", e.getMessage(), e);
         }
     }
 
+    /**
+     * 释放资源
+     */
+    private static void close() {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                log.error("ResultSet对象释放失败：{}", e.getMessage(), e);
+            }
+        }
+        if (pstmt != null) {
+            try {
+                pstmt.close();
+            } catch (SQLException e) {
+                log.error("PreparedStatement对象释放失败：{}", e.getMessage(), e);
+            }
+        }
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                log.error("Connection对象释放失败：{}", e.getMessage(), e);
+            }
+        }
+    }
+
+    /**
+     * select
+     *
+     * @return 查询结果集
+     */
     public static ResultSet query(String sql, Object... args) {
         try {
             pstmt = con.prepareStatement(sql);
@@ -54,11 +88,18 @@ public class JdbcUtils {
             }
             rs = pstmt.executeQuery();
         } catch (SQLException e) {
-            e.printStackTrace();
+            log.error("SQL查询失败：{}", e.getMessage(), e);
+        } finally {
+            close();
         }
         return rs;
     }
 
+    /**
+     * insert, update, delete
+     *
+     * @return 影响的行属
+     */
     public static int update(String sql, Object... args) {
         try {
             pstmt = con.prepareStatement(sql);
@@ -67,9 +108,25 @@ public class JdbcUtils {
             }
             return pstmt.executeUpdate();
         } catch (SQLException e) {
-            e.printStackTrace();
+            log.error("DML执行失败：{}", e.getMessage(), e);
+        } finally {
+            close();
         }
         return 0;
+    }
+
+    /**
+     * 可以用于执行任何SQL语句，一般用于执行DDL语句
+     */
+    public static void execute(String sql, String... args) {
+        try {
+            // execute（）只有执行select等带有ResultSet object返回结果集时它的返回值才是true
+            pstmt.execute(sql, args);
+        } catch (SQLException e) {
+            log.error("SQL执行失败：{}", e.getMessage(), e);
+        } finally {
+            close();
+        }
     }
 
 }
--- src/main/java/org/pcdd/javase/tutorial/chapter20_数据库编程/Main.java
+++ src/main/java/org/pcdd/javase/tutorial/chapter20_数据库编程/Main.java
@@ -1,27 +1,29 @@
 package org.pcdd.javase.tutorial.chapter20_数据库编程;
 
-import java.sql.ResultSet;
-
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        long t1 = System.currentTimeMillis();
-        JdbcUtils.init();
+        // 查询
+        System.out.println(JdbcUtils.query("select * from student where name = ?",
+                "王明阳"));
 
-        ResultSet rs = JdbcUtils.query("select * from course");
-        while (rs.next()) {
-            System.out.println(rs.getString(1) + "\t"
-                    + rs.getString(2) + "\t"
-                    + rs.getString(3) + "\t"
-            );
-        }
+        // 添加
+        System.out.println(JdbcUtils.update("insert into student values(?,?,?,?)",
+                "5672", "测试姓名", "测试班级", "测试院系"));
 
-        int i = JdbcUtils.update("update student set Sname = ? where Sno = ?", "张山峰", 202001);
-        System.out.println("i = " + i);
+        // 修改
+        System.out.println(JdbcUtils.update("update student set clazz = ? where sno = ?",
+                "计算机科学与技术1841", "5672"));
 
-        long t2 = System.currentTimeMillis();
-        System.out.println("总耗时：" + (t2 - t1) + "ms");
-        System.out.println(JdbcUtils.ds);
+        // 删除
+        System.out.println(JdbcUtils.update("delete from student where sno = ?",
+                "5672"));
+
+        // 创建test表
+        JdbcUtils.execute("create table jdbc_test(id char(10) primary key)");
+
+        // 将字段id重命名为sid，并修改数据类型为int
+        JdbcUtils.execute("alter table jdbc_test change id sid int");
     }
 
 }
